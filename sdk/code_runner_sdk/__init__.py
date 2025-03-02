a855633837a2dfa2b1e3bfb1425b71bfe2ca18c6
gaozhirong <gaozhirong@thunisoft.com>
2025-03-02 16:58:03 +0800

    feat(): update sdk

--- sdk/code_runner_sdk/__init__.py
+++ sdk/code_runner_sdk/__init__.py
@@ -5,9 +5,10 @@ Code Runner SDK for Python
 一个用于与Code Runner服务进行交互的Python SDK。
 """
 
-__version__ = '0.1.0'
+__version__ = '0.1.1'
 
 from .core.client import CodeRunnerClient
+from .core.config import CodeRunnerConfig
 from .models.code_execution import (
     CodeExecutionRequest,
     CodeExecutionResponse,
--- sdk/setup.py
+++ sdk/setup.py
@@ -2,20 +2,28 @@
 Code Runner Python SDK安装配置
 """
 from setuptools import setup, find_packages
+import os
 
-with open("README.md", "r", encoding="utf-8") as fh:
+with open("code_runner_sdk/README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
 
+# 确保包含所有Python模块
+packages = find_packages(include=['code_runner_sdk', 'code_runner_sdk.*'])
+
 setup(
-    name="code-runner-sandbox-sdk",
-    version="0.1.0",
+    name="code-runner-sdk",
+    version="0.1.1",
     author="calm",
     author_email="losergzr@gmail.com",
     description="Code Runner Sandbox服务的Python SDK",
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/shouldnotappearcalm/Code-Runner-Sandbox",
-    packages=find_packages(),
+    packages=packages,
+    package_data={
+        'code_runner_sdk': ['**/*.py'],  # 包含所有Python文件
+    },
+    include_package_data=True,
     classifiers=[
         "Development Status :: 3 - Alpha",
         "Intended Audience :: Developers",
